9d2c297338d97ea5f4cd44473a30444e329dc66f
husnainkhan620 <husain_khan2000@yahoo.com>
2025-06-08 19:13:13 -0600

    adding ERDiagram updates

--- src/main/java/com/openfx/erdiagram/TitledPaneNode.java
+++ src/main/java/com/openfx/erdiagram/TitledPaneNode.java
@@ -3,7 +3,16 @@ package com.openfx.erdiagram;
 import java.util.Comparator;
 import java.util.TreeSet;
 
+import javafx.geometry.Insets;
+import javafx.geometry.Pos;
+import javafx.scene.control.Label;
 import javafx.scene.control.TitledPane;
+import javafx.scene.control.Tooltip;
+import javafx.scene.layout.Background;
+import javafx.scene.layout.BackgroundFill;
+import javafx.scene.layout.CornerRadii;
+import javafx.scene.layout.VBox;
+import javafx.scene.paint.Color;
 
 class TitlePaneComparator implements Comparator<TitledPaneNode> {
 
@@ -21,6 +30,28 @@ public class TitledPaneNode extends TitledPane {
 	TreeSet<TitledPaneNode> rigtRelatedTitledPanes = new TreeSet<TitledPaneNode>(new TitlePaneComparator());
 	TreeSet<TitledPaneNode> bottomRelatedTitledPanes = new TreeSet<TitledPaneNode>(new TitlePaneComparator());
 	TreeSet<TitledPaneNode> leftRelatedTitledPanes = new TreeSet<TitledPaneNode>(new TitlePaneComparator());
+	
+	public TitledPaneNode() {
+		
+	}
+	
+	public TitledPaneNode(String entityName, Tooltip tooltip,double layoutX,double layoutY,String[] entityAttributes) {
+		// TODO Auto-generated constructor stub
+		this.setText(entityName);
+		this.setTooltip(tooltip);
+		this.alignmentProperty().setValue(Pos.CENTER);
+		
+		VBox titledPaneVBox = new VBox();
+		titledPaneVBox.setBackground(new Background(  new BackgroundFill(Color.AZURE,  CornerRadii.EMPTY,Insets.EMPTY )));
+		titledPaneVBox.setSpacing(3);		
+		for(int i=0;i<entityAttributes.length;i++) {
+			titledPaneVBox.getChildren().add(new Label(entityAttributes[i]));
+		}				
+		this.setContent(titledPaneVBox);
+		this.setCollapsible(false);
+		this.setLayoutX(layoutX);   // used to place it a particular X location on screen
+		this.setLayoutY(layoutY);   // used to place it a particular Y location on screen
+	}
 	
 //	private TitledPane titledPane;
 	/*
@@ -57,6 +88,8 @@ public class TitledPaneNode extends TitledPane {
 	
 	
 	
+
+
 	public boolean equals(Object object) {
 		
 		TitledPaneNode titledPaneNode = (TitledPaneNode)object;
